ba4595c82ab688045ec7d9405ee0be671074f7ba
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-06 18:38:47 +0300

    ///

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('#form'); // знаходимо форму
 const taskInput = document.querySelector('#taskInput'); // знаходимо Task Input
 
-form.addEventListener('submit', function (event) { // за допомогою цього, можемо для кнопки "Додати" відіграти якусь дію
+form.addEventListener('submit', function (event) {
+  // за допомогою цього, можемо для кнопки "Додати" відіграти якусь дію
   event.preventDefault();
+
+  const taskText = taskInput.ariaValueMax;
+  console.log(taskText);
 });
