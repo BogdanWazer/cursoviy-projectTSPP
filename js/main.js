6eb277697f55db0a03fcd75c35cc5b6a78439c16
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-06 20:29:02 +0300

    22

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 const form = document.querySelector('#form'); // знаходимо форму
 const taskInput = document.querySelector('#taskInput'); // знаходимо Task Input
+const tasksList = document.querySelector('#tasksList');
 
 form.addEventListener('submit', function (event) {
   // за допомогою цього, можемо для кнопки "Додати" відіграти якусь дію
   event.preventDefault();
 
-  const taskText = taskInput.ariaValueMax;
-  console.log(taskText);
+  // Ми отримаємо назву задачі
+  const taskText = taskInput.value;
+
+  // Робимо нову розмітку для задачі
+
+  const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
+            <span class="task-title">${taskText}</span>
+            <div class="task-item__buttons">
+              <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18" />
+              </button>
+              <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18" />
+              </button>
+            </div>
+          </li>`;
+
+  tasksList.insertAdjacentHTML('beforeend', taskHTML);
 });
