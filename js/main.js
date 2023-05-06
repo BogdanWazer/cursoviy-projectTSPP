dff98b36bcd47605bd2d62e4187eec2f43aeb8c7
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-05-06 18:18:47 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const form = document.querySelector('#form'); // знаходимо форму
+const taskInput = document.querySelector('#taskInput'); // знаходимо Task Input
+
+form.addEventListener('submit', function (event) { // за допомогою цього, можемо для кнопки "Додати" відіграти якусь дію
+  event.preventDefault();
+});
