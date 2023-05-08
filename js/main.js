07f1233ebb5664877ea5d5ed5c6662d7b6d2f5c5
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-08 13:08:51 +0300

    tasksdone

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ const taskInput = document.querySelector('#taskInput'); // знаходимо Ta
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
+//Робота з данними
+let tasks = [];
+
+// Якщо натиснуто - додати задачу
 form.addEventListener('submit', addTask);
 
+// якщо клік - Видалити задачу
 tasksList.addEventListener('click', deleteTask);
 
-//Задача завершена
+// якщо клік - Задача виконана
 tasksList.addEventListener('click', doneTask);
 
 function addTask(event) {
@@ -17,10 +22,24 @@ function addTask(event) {
   // Ми отримаємо назву задачі
   const taskText = taskInput.value;
 
+  // Створюємо обʼєкт, що містить данні про нашу нову задачу
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false,
+  };
+
+  tasks.push(newTask);
+
+  console.log(tasks);
+
+  //Формуємо CSS class
+  const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
+
   // Робимо нову розмітку для задачі
 
-  const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-            <span class="task-title">${taskText}</span>
+  const taskHTML = `<li id="${newTask.id} "class="list-group-item d-flex justify-content-between task-item">
+            <span class="${cssClass}">${newTask.text}</span>
             <div class="task-item__buttons">
               <button type="button" data-action="done" class="btn-action">
                 <img src="./img/tick.svg" alt="Done" width="18" height="18" />
@@ -42,27 +61,41 @@ function addTask(event) {
 
 function deleteTask(event) {
   // Якщо кнопка не натиснута = виходимо із функції
-  if (event.target.dataset.action !== 'delete') return;
-
+  if (event.target.dataset.action !== 'delete') {
+    return;
+  }
   const parentNode = event.target.closest('.list-group-item');
+
+  // Визначаємо ID задачі
+  const id = Number(parentNode.id);
+
+  // // Знаходимо індекс задачі в масиві
+  // const index = tasks.findIndex((task) => task.id === id);
+
+  // // Видаляємо задачу з масиву задач
+
+  // tasks.splice(index, 1);
+
+  tasks = tasks.filter((task) => task.id !== id);
+
+  // Видаляємо задачі з розмітки
   parentNode.remove();
 
   // Перевіряємо чи є інші задачі в списку
 
   if (tasksList.children.length > 1) {
-    emptyList.classList.add('none'); // назначаємо display:none для "Список справ порожній"
+    emptyList.classList.add('none');
+    // назначаємо display:none для "Список справ порожній"
     // Повертаємо HTML колекцію із тегів li
   }
 }
 
 // Функціонал кнопки "Готово"
 function doneTask(event) {
-  
   // Якщо кнопка не натиснута = виходимо із функції
   if (event.target.dataset.action !== 'done') return;
 
   const parentNode = event.target.closest('.list-group-item');
   const taskTitle = parentNode.querySelector('.task-title');
   taskTitle.classList.toggle('task-title--done');
-  console.log(taskTitle);
 }
