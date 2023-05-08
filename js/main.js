a3de0c529dd99dd23c26eea734ec20476365113c
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-08 16:15:09 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ const emptyList = document.querySelector('#emptyList');
 //Робота з данними
 let tasks = [];
 
+// Парсинг елементів через JSON
+
+if (localStorage.getItem('tasks')) {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.forEach((task) => renderTask(task));
+}
+
+checkEmptyList();
+
 // Якщо натиснуто - додати задачу
 form.addEventListener('submit', addTask);
 
@@ -28,34 +37,19 @@ function addTask(event) {
     text: taskText,
     done: false,
   };
-
+  //Додавання задачі в NewTask
   tasks.push(newTask);
+  //Додавання задачі в LocalStorage
+  saveToLocalStorage();
 
-  console.log(tasks);
-
-  //Формуємо CSS class
-  const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
-
-  // Робимо нову розмітку для задачі
-
-  const taskHTML = `<li id="${newTask.id} "class="list-group-item d-flex justify-content-between task-item">
-            <span class="${cssClass}">${newTask.text}</span>
-            <div class="task-item__buttons">
-              <button type="button" data-action="done" class="btn-action">
-                <img src="./img/tick.svg" alt="Done" width="18" height="18" />
-              </button>
-              <button type="button" data-action="delete" class="btn-action">
-                <img src="./img/cross.svg" alt="Done" width="18" height="18" />
-              </button>
-            </div>
-          </li>`;
-
-  tasksList.insertAdjacentHTML('afterbegin', taskHTML);
+  //Рендеринг завдань перед стартом роботи
+  renderTask(newTask);
 
   // Очищуємо поле для вводу та повертаємо на нього фокус.
 
   taskInput.value = '';
   taskInput.focus();
+  checkEmptyList();
 }
 // Функціонал кнопки "Видалити"
 
@@ -81,13 +75,7 @@ function deleteTask(event) {
   // Видаляємо задачі з розмітки
   parentNode.remove();
 
-  // Перевіряємо чи є інші задачі в списку
-
-  if (tasksList.children.length > 1) {
-    emptyList.classList.add('none');
-    // назначаємо display:none для "Список справ порожній"
-    // Повертаємо HTML колекцію із тегів li
-  }
+  checkEmptyList();
 }
 
 // Функціонал кнопки "Готово"
@@ -96,6 +84,58 @@ function doneTask(event) {
   if (event.target.dataset.action !== 'done') return;
 
   const parentNode = event.target.closest('.list-group-item');
+
+  const id = Number(parentNode.id);
+
+  const task = tasks.find((task) => task.id === id);
+  //Змінюємо статус на зворотній
+  task.done = !task.done;
+
+  saveToLocalStorage();
+
+  console.log(task);
+
   const taskTitle = parentNode.querySelector('.task-title');
   taskTitle.classList.toggle('task-title--done');
+
+  checkEmptyList();
+}
+
+function checkEmptyList() {
+  if (tasks.length === 0) {
+    const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+            <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3" />
+            <div class="empty-list__title">Список справ порожній</div>
+          </li>`;
+    tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+  }
+
+  if (tasks.length > 0) {
+    const emptyListEl = document.querySelector('#emptyList');
+    emptyListEl ? emptyListEl.remove() : null;
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+  const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+  // Робимо нову розмітку для задачі
+
+  const taskHTML = `<li id="${task.id} "class="list-group-item d-flex justify-content-between task-item">
+            <span class="${cssClass}">${task.text}</span>
+            <div class="task-item__buttons">
+              <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18" />
+              </button>
+              <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18" />
+              </button>
+            </div>
+          </li>`;
+
+  tasksList.insertAdjacentHTML('afterbegin', taskHTML);
 }
